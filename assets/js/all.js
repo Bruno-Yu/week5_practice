086492aef6f9e788b8a12c2bf5940eceede98a9d
Bruno Yu <jackhellowin@gmail.com>
2022-08-07 12:39:53 +0800

    Update 2022-08-07T04:39:47.482Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,14 +1,14 @@
+"use strict";
+
 // const Modal = require('../../../node_modules/bootstrap/js/src/modal');
 // // const modal = new Modal(document.getElementById('myModal'))
-
 // const visibleBtns = document.querySelectorAll(' [data-bs-toggle="modal"]');
 // const exampleModal= new Modal(document.querySelector(' #exampleModal'));
 // visibleBtns.addEventListener('click', (e) => { 
 //   exampleModal.show();
 // })
-
 console.log('Hello!');
-
-$(document).ready(() => {
+$(document).ready(function () {
   console.log('HesSchool Hello!');
 });
+//# sourceMappingURL=all.js.map
