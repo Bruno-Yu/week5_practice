981892399fb8ab4578cf4ff7c34bd0d399f001c6
Bruno Yu <jackhellowin@gmail.com>
2022-08-07 10:49:05 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,14 @@
+// const Modal = require('../../../node_modules/bootstrap/js/src/modal');
+// // const modal = new Modal(document.getElementById('myModal'))
+
+// const visibleBtns = document.querySelectorAll(' [data-bs-toggle="modal"]');
+// const exampleModal= new Modal(document.querySelector(' #exampleModal'));
+// visibleBtns.addEventListener('click', (e) => { 
+//   exampleModal.show();
+// })
+
+console.log('Hello!');
+
+$(document).ready(() => {
+  console.log('HesSchool Hello!');
+});
