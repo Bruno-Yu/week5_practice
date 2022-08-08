9aa933b2d7c8c38ae1d862241a42935341f57b65
Bruno Yu <jackhellowin@gmail.com>
2022-08-08 14:16:51 +0800

    prettier format version easier for reading

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,12 +3,12 @@
 
 // const visibleBtns = document.querySelectorAll(' [data-bs-toggle="modal"]');
 // const exampleModal= new Modal(document.querySelector(' #exampleModal'));
-// visibleBtns.addEventListener('click', (e) => { 
+// visibleBtns.addEventListener('click', (e) => {
 //   exampleModal.show();
 // })
 
-console.log('Hello!');
+console.log("Hello!");
 
 $(document).ready(() => {
-  console.log('HesSchool Hello!');
+  console.log("HesSchool Hello!");
 });
